fc6e2f353a70ef3dd4a669f64ddce6b020e0be3e
Prithvi101 <pruthvik41@hotmail.com>
2023-08-15 19:00:05 +0530

    added add note functionally with error handeling

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ yargs.command({
         }
     },
     handler: function(argv){
-        addNote
+        addNote(argv.title,argv.body)
     }
 })
 //remove
--- notes.js
+++ notes.js
@@ -1,13 +1,39 @@
+const fs = require('fs')
 const getNotes = function () {
     return 'Your notes...'
 }
-const addNote = function(title,body){
+const addNote = function (title, body) {
+    const notes = loadNotes()
+    if(notes.every((note)=>(note.title!=title))){
+        notes.push({
+            title:title,
+            body:body,
+        })
+        saveNote(notes)
+        console.log("Note Added Sucessfully !!!")
+    }
+    else{
+        console.log("Note title taken!")
+    }
+
 
 }
-
-
+const saveNote = function(notes){
+    const data = JSON.stringify(notes)
+    fs.writeFileSync('notes.json',data)
+}
+const loadNotes = function () {
+    try {
+        const db = JSON.parse(fs.readFileSync('notes.json').toString())
+        //console.log(db)
+        return db
+    } catch (error) {
+        console.log("creating new File")
+        return []
+    }
+}
 
 module.exports = {
     getNotes: getNotes,
-    addNote: addNote,
+    addNote: addNote
 }
