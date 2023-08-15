9e8a180242332841d24281b0f86d719fe0768877
Prithvi101 <pruthvik41@hotmail.com>
2023-08-15 18:36:50 +0530

    on add notes method

---
+++ app.js
@@ -0,0 +1,68 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const {getNotes,addNote} = require('./notes.js')
+const fs = require('fs')
+const command = process.argv[2]
+
+
+// add , remove , read , list
+//add
+yargs.command({
+    command:'add',
+    describe:'Adding a Note',
+    builder:{
+        title:{
+            describe:"Note Title",
+            demandOption:true,
+            type:'string',
+        },
+        body:{
+            describe:"Note Body",
+            demandOption:true,
+            type:'string',
+        }
+    },
+    handler: function(argv){
+        addNote
+    }
+})
+//remove
+yargs.command({
+    command:'remove',
+    describe:'removing a Note',
+    builder:{
+        title:{
+            describe:""
+        }
+    },
+    handler: function(){
+        console.log("Removing Note..");
+    }
+})
+//read
+yargs.command({
+    command:'read',
+    describe:'Reading a Note',
+    builder:{
+        title:{
+            describe:""
+        }
+    },
+    handler: function(){
+        console.log("Reading Note..");
+    }
+})
+//list
+yargs.command({
+    command:'list',
+    describe:'Listing a Note',
+    builder:{
+        title:{
+            describe:""
+        }
+    },
+    handler: function(){
+        console.log("Listing Note..");
+    }
+})
+yargs.parse()
---
+++ notes.js
@@ -0,0 +1,13 @@
+const getNotes = function () {
+    return 'Your notes...'
+}
+const addNote = function(title,body){
+
+}
+
+
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+}
